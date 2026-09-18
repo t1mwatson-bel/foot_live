eb4386f880ba13a53faaadabeb9fa5f8dae7d21f
t1mwatson-bel <mix3005@yandex.ru>
2026-09-18 22:14:01 +0300

    Update foot.py

--- foot.py
+++ foot.py
@@ -25,7 +25,7 @@ print(f"✅ BOT_TOKEN: {BOT_TOKEN[:5]}...", flush=True)
 print(f"✅ CHAT_ID: {CHAT_ID}", flush=True)
 
 MOSCOW_TZ = pytz.timezone('Europe/Moscow')
-BASE_URL = "https://1xlite-36553.pro"
+BASE_URL = "https://1xlite-7720.pro"
 API = f"https://api.telegram.org/bot{BOT_TOKEN}"
 
 # =====================================================================
